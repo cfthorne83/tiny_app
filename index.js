0c3a4c7e02648f3b8809f2c0936d8821804fe91b
cthorne <cfthorne83@gmail.com>
2021-09-16 15:02:18 -0400

    js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const slides = [
+const app = [
     'app', 'blog', 'shop', 'note', 'game', 'grave'
 ]
-let app = document.querySelector("app");
-let blog = document.querySelector("blog");
-let shop = document.querySelector("shop");
-let note = document.querySelector("note");
-let game = document.querySelector("game");
-let grave = document.querySelector("grave");
-document.addEventListener("DOMContentLoaded",  )
+
+let appName = document.getElementById('app-name');
+
+// let app = document.querySelector("app");
+// let blog = document.querySelector("blog");
+// let shop = document.querySelector("shop");
+// let note = document.querySelector("note");
+// let game = document.querySelector("game");
+// let grave = document.querySelector("grave");
+// document.addEventListener("DOMContentLoaded",  );
