e15273d7eb183ef67133d72d1af7e7405139ac61
cthorne <cfthorne83@gmail.com>
2021-09-16 15:38:26 -0400

    delete dead code2

--- index.js
+++ index.js
@@ -33,10 +33,3 @@ const rotateLeft = () => {
 
 document.getElementById('right-button').onclick = rotateRight;
 document.getElementById('left-button').onclick = rotateLeft;
-
-// let app = document.querySelector("app");
-// let blog = document.querySelector("blog");
-// let shop = document.querySelector("shop");
-// let note = document.querySelector("note");
-// let game = document.querySelector("game");
-// let grave = document.querySelector("grave");
