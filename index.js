d922a386f16187bc7776aff082d5b373c6d6be0b
cthorne <cfthorne83@gmail.com>
2021-09-16 14:45:52 -0400

    more js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const slides = [
     'app', 'blog', 'shop', 'note', 'game', 'grave'
 ]
-
+let app = document.querySelector("app");
+let blog = document.querySelector("blog");
+let shop = document.querySelector("shop");
+let note = document.querySelector("note");
+let game = document.querySelector("game");
+let grave = document.querySelector("grave");
 document.addEventListener("DOMContentLoaded",  )
